fb756e6fdab3302849ca02020f5b6f71eeeaae4b
kiran <kiran@DESKTOP-GA8TP1V>
2023-01-28 19:48:35 +0530

    store resored

--- src/actions/store.js
+++ src/actions/store.js
@@ -1,48 +1,12 @@
-export const FETCH_MEASUREMENTTYPE = "FETCH_MEASUREMENTTYPE";
-export const FETCH_MEASUREMENTTYPE_SUCCESS = "FETCH_MEASUREMENTTYPE_SUCCESS";
-export const FETCH_MEASUREMENTTYPE_FAILURE = "FETCH_MEASUREMENTTYPE_FAILURE";
-export const ADD_MEASUREMENTTYPE = "ADD_MEASUREMENTTYPE";
-export const UPDATE_MEASUREMENTTYPE = "UPDATE_MEASUREMENTTYPE";
-export const DELETE_MEASUREMENTTYPE = "DELETE_MEASUREMENTTYPE";
-export const MEASUREMENTTYPE = "MEASUREMENTTYPE";
-
-export const getMeasurementTypeBeginAction = () => ({
-    type: FETCH_MEASUREMENTTYPE,
-})
-export const getMeasurementTypeSuccessAction = (data) => ({
-    type: FETCH_MEASUREMENTTYPE_SUCCESS,
-    payload:data,
-})
-export const getMeasurementTypeFailureAction = (error) => ({
-    type: FETCH_MEASUREMENTTYPE_FAILURE,
-    payload:{error},
-})
-export const addMeasurementTypeAction = (data) => ({
-    type: ADD_MEASUREMENTTYPE,
-    payload: data,
-})
-export const measurementTypeAction = (data) => ({
-    type: MEASUREMENTTYPE,
-    payload: data,
-})
-
-export const updateMeasurementTypeAction = (data) => ({
-    type: UPDATE_MEASUREMENTTYPE,
-    payload: data,
-})
-export const deleteMeasurementTypeAction = (data) => ({
-    type: DELETE_MEASUREMENTTYPE,
-    payload: data,
-})
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { reducers } from "../reducers";
 
 
-
-
-
-
-
-
-
-
-
-
+export const store = createStore(
+    reducers,
+    compose(
+        applyMiddleware(thunk),
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
