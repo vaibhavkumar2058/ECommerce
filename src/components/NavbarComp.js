1a1a3d0ba2c08aa86aaff6da3bf178f87e4dac78
Zaid-Shadab <jarks.D8@outlook.com>
2023-01-28 15:53:27 +0530

    implemented file,product

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -9,15 +9,20 @@ import {
 } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Enquiry from "../pages/Enquiry";
-
+import CategoryTypes from "../pages/CategoryType";
+import Products from "../pages/Product";
+import Files from "../pages/File";
 
 
 const NavbarComp = () => {
   return (
-    <Router>
+    <Router>              
       <div>
         <Routes>
-          <Route exact path="/enquiry" element={<Enquiry />} />           
+          <Route exact path="/enquiry" element={<Enquiry />} /> 
+          <Route exact path="/categoryTypes" element={<CategoryTypes/>} />
+          <Route exact path="/products" element={<Products/>} /> 
+          <Route exact path="/files" element={<Files/>} />          
        </Routes>
       </div>
     </Router>
