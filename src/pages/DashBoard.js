9a54606453cb5c5c5f3e2f07364e71861e05b94a
kiran <kiran@DESKTOP-GA8TP1V>
2023-02-25 09:06:49 +0530

    added charts

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import useFetchGMT from "../hooks/useFetchGMT";
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart,
+    Area, BarChart, Bar, PieChart, Pie, Cell
+} from 'recharts';
 
 export default function Dashboard() {
 
@@ -12,9 +16,60 @@ export default function Dashboard() {
         recordStatusId: 1,
     });
 
-    const { 
+    const pdata = [
+        {
+            name: 'Python',
+            student: 13,
+            fees: 10
+        },
+        {
+            name: 'Javascript',
+            student: 15,
+            fees: 12
+        },
+        {
+            name: 'PHP',
+            student: 5,
+            fees: 10
+        },
+        {
+            name: 'Java',
+            student: 10,
+            fees: 5
+        },
+        {
+            name: 'C#',
+            student: 9,
+            fees: 4
+        }
+    ];
+
+    const pieData = [
+        {
+            name: "Detergent Soaps",
+            value: 54
+        },
+        {
+            name: "Detergent Liquids",
+            value: 47
+        },
+        {
+            name: "Dishwash Liquids",
+            value: 16
+        },
+        {
+            name: "Dishwash Soaps",
+            value: 16
+        },
+        {
+            name: "Adipranav Agarbathi",
+            value: 10
+        }
+    ];
+
+    const {
         addGMT,
-      } = useFetchGMT();
+    } = useFetchGMT();
 
     const [messageStatus, setMessageStatus] = useState({
         mode: "",
@@ -23,13 +78,13 @@ export default function Dashboard() {
         message: "",
     });
 
-    setInterval(() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            newGMT.latitude = position.coords.latitude;
-            newGMT.longitude = position.coords.longitude;
-            saveHandler();
-        });
-    }, 100000);
+    // setInterval(() => {
+    //     navigator.geolocation.getCurrentPosition((position) => {
+    //         newGMT.latitude = position.coords.latitude;
+    //         newGMT.longitude = position.coords.longitude;
+    //         saveHandler();
+    //     });
+    // }, 100000);
 
     const saveHandler = async () => {
         debugger;
@@ -49,10 +104,80 @@ export default function Dashboard() {
         }
     }
 
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+
+    const COLORS = ["#8884d8", "#82ca9d", "#FFBB28", "#FF8042", "#AF19FF"];
+    const CustomTooltip = ({ active, payload, label }) => {
+        if (active) {
+            return (
+                <div
+                    className="custom-tooltip"
+                    style={{
+                        backgroundColor: "#ffff",
+                        padding: "5px",
+                        border: "1px solid #cccc"
+                    }}
+                >
+                    <label>{`${payload[0].name} : ${payload[0].value}%`}</label>
+                </div>
+            );
+        }
+    }
+
     return (
         <>
-            <div className="m-t-40">
-                Dashboard
+            Dashboard
+            <div className="row">
+                <div className="col-md-6">
+                    <PieChart width={500} height={300}>
+                        <Pie
+                            data={pieData}
+                            color="#000000"
+                            dataKey="value"
+                            nameKey="name"
+                            cx="50%"
+                            cy="50%"
+                            labelLine={false}
+                            outerRadius={120}
+                            fill="#8884d8"
+                            label={renderCustomizedLabel}
+                        >
+                            {pieData.map((entry, index) => (
+                                <Cell
+                                    key={`cell-${index}`}
+                                    fill={COLORS[index % COLORS.length]}
+                                />
+                            ))}
+                        </Pie>
+                        <Tooltip />
+                        <Legend />
+                    </PieChart>
+                </div>
+                <div className="col-md-6">
+                    <LineChart width={500} height={300} data={pdata}>
+                        <Line type="monotone" dataKey="student" stroke="#8884d8"
+                            strokeWidth={4} activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="fees" stroke="orange"
+                            strokeWidth={4} activeDot={{ r: 8 }} />
+                        <XAxis dataKey="name" interval={'preserveStartEnd'} tickFormatter={(value) => value} />
+                        <YAxis />
+                        <Tooltip contentStyle={{ backgroundColor: 'yellow' }} />
+                        <Legend />
+                    </LineChart>
+                </div>
+
             </div>
         </>
     );
