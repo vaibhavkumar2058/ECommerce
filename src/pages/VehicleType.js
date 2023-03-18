4400f2e59b1efecff33de956f22b8aa65a986aca
Alfiya <jarks.d3@outlook.com>
2023-03-18 16:55:26 +0530

    test alfiya

--- src/pages/VehicleType.js
+++ src/pages/VehicleType.js
@@ -163,7 +163,7 @@ export default function VehicleType() {
   const getRecordStatusList = async () => {
     const response = await getRecordStatuss();
     if (response.payload.title == "Success") {
-alert();
+
       var arr = [];
       for (var key in response.payload) {
         arr.push(response.payload[key]);
