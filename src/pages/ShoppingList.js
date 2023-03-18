7ed55c4c57ed58b8844bd26b3840c119f85cdde2
Yunaz Shaik <jarks.d9@outlook.com>
2023-03-18 12:18:08 +0530

    yunaz

--- src/pages/ShoppingList.js
+++ src/pages/ShoppingList.js
@@ -82,7 +82,7 @@ export default function ShoppingList() {
           <div class="row">
             <div class="col-md-3 col-lg-3"></div>
             <div class="col-md-6 col-lg-6"><b><h1>Shopping Cart</h1></b></div>
-            <div class="col-md-3 col-lg-3"><h6>Subtotal 5 items:200</h6></div>
+           
           </div>
           <div class="row">
 
